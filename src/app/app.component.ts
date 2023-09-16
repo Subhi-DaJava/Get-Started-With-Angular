03ab0baed3f2d57e3220165fd4e5927c6516f5eb
Subhi YD <subhy809fr@gmail.com>
2023-09-16 21:34:48 +0200

    v1.5.0 add location optional faceSnap model, add directif ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,29 @@ export class AppComponent implements OnInit{
   lastSnap!: FaceSnap;
   
   ngOnInit(): void {
-    this.mySnap = new FaceSnap (
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-   ),
-   this.otherSnap = new FaceSnap (
-    'Three Rock Mountain',
-    'Un endroit magnifique pour les rendonnées.',
-    'https://upload.wikimedia.org/wikipedia/commons/0/08/Three_Rock_Mountain_Southern_Tor.jpg',
-    new Date(),
-    0
-   ),
-   this.lastSnap = new FaceSnap (
-    'Un bon repas',
-    'Mmmh que c\'est bon !',
-    'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-    new Date(),
-    0
- )
+    this.mySnap = { 
+      title :'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 8,
+      location: 'Paris'
+  },
+   this.otherSnap = {
+    title: 'Three Rock Mountain',
+    description: 'Un endroit magnifique pour les rendonnées.',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Three_Rock_Mountain_Southern_Tor.jpg',
+    createdDate: new Date(),
+    snaps: 7,
+    location: 'la Montaigne'
+   },
+   this.lastSnap = { 
+    title: 'Un bon repas',
+    description: 'Mmmh que c\'est bon !',
+    imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+    createdDate: new Date(),
+    snaps: 4,
+
+   }
   }
 }
