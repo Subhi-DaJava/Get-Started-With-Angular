fe702289a0cc95442e78d8f576334e4f11340619
Subhi YD <subhy809fr@gmail.com>
2023-09-17 12:52:15 +0200

    v.1.6.0 create FaceSnap array, show with *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,61 @@ import { FaceSnap } from './models/face-snap.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  mySnap!: FaceSnap;
-  otherSnap!: FaceSnap;
-  lastSnap!: FaceSnap;
-  
-  ngOnInit(): void {
-    this.mySnap = { 
-      title :'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 8,
-      location: 'Paris'
-  },
-   this.otherSnap = {
-    title: 'Three Rock Mountain',
-    description: 'Un endroit magnifique pour les rendonnées.',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Three_Rock_Mountain_Southern_Tor.jpg',
-    createdDate: new Date(),
-    snaps: 7,
-    location: 'la Montaigne'
-   },
-   this.lastSnap = { 
-    title: 'Un bon repas',
-    description: 'Mmmh que c\'est bon !',
-    imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-    createdDate: new Date(),
-    snaps: 4,
+export class AppComponent implements OnInit {
+  // mySnap!: FaceSnap;
+  // otherSnap!: FaceSnap;
+  // lastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
-   }
+  ngOnInit(): void {
+
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 8,
+        location: 'Paris'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les rendonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 7,
+        location: 'la Montaigne'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 4,
+      },
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 8,
+        location: 'Paris'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les rendonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 7,
+        location: 'la Montaigne'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 4,
+      }
+    ];
   }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -6,27 +6,27 @@ import { FaceSnap } from '../models/face-snap.model';
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent implements OnInit{
+export class FaceSnapComponent implements OnInit {
 
   @Input() faceSnap!: FaceSnap;
-  
+
   buttonText!: string;
-  
+
   ngOnInit(): void {
-   // throw new Error('Method not implemented.');
-   this.buttonText = "Oh Snap!";
+    // throw new Error('Method not implemented.');
+    this.buttonText = "Oh Snap!";
   }
 
   onSnap() {
 
-    if(this.buttonText === 'Oh Snap!') {
+    if (this.buttonText === 'Oh Snap!') {
       this.faceSnap.snaps++;
       this.buttonText = 'Oops, OneSnap!';
     } else {
-      
+
       this.faceSnap.snaps--;
       this.buttonText = 'Oh Snap!';
     }
-    
-    }
+
+  }
 }
